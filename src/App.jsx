5173719cbaf5df36a8aa653aa24d933c29b1e3b9
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2023-10-29 12:01:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import SearchParams from './SearchParams'
+import './style.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
+
+}
+
+export default App
